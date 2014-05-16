03361e8e0462c7228efeb4b560da1110492fa3f8
Milos Milanovic <milos.milanovic@rt-rk.com>
2014-05-16 14:58:33 +0200

    Removed project.

--- DtvExampleOne/src/com/iwedia/dtv/DVBStatusCallBack.java
+++
@@ -1,9 +0,0 @@
-package com.iwedia.dtv;
-
-/**
- * CallBack for currently DVB status.
- */
-public interface DVBStatusCallBack {
-    /** Alert UI that channel is scrambled. */
-    public void channelIsScrambled();
-}
